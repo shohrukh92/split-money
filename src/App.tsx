f6449baf043210fa970c3f5fbba30335008e2a40
Shohrukh <shohrukh.92@gmail.com>
2018-12-23 21:30:12 +0300

    Condif additional tslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="app-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
